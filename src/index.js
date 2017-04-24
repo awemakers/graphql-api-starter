73091290ae45c49d213d5e7847f785e8879640f5
baptistelambert <bapthf2@gmail.com>
2017-04-25 00:11:39 +0200

    feat(cors): add cors middleware

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import 'dotenv/config';
 import 'database/mongo';
@@ -15,9 +16,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
 getSchema().then((schema) => {
-  app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+  app.use('/graphql', graphqlExpress({ schema }));
   app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-  app.listen(PORT);
-  console.log(`Server listening on ${PORT}`);
+  app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
 }).catch(err => console.log(err));
